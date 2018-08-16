268f7b6bf1a345ad516560cbb1253eca6665442d
Bruno de Sá <bcunhasa@gmail.com>
2018-08-16 18:06:54 -0300

    Adição de questões relativas às informações pessoais e gerais

--- administracao/const.py
+++ administracao/const.py
@@ -2,16 +2,63 @@
 
 TAMANHO_NOMES = 200
 
-# Constantes do tipo Aluno
+
+# Constantes do modelo Aluno
 
 SEXO = (
-    ('0', 'FEMININO'),
-    ('1', 'MASCULINO')
+    ('0', 'Feminino'),
+    ('1', 'Masculino')
 )
 
-# Constantes do questionário
+ESCOLA = (
+    ('0', 'Escola municipal Antônio Carlos Jobim')
+    ('1', 'Escola municipal Antônio G. de Carvalho Filho')
+    ('2', 'Escola municipal Anne Frank')
+    ('3', 'Escola municipal Darcy Ribeiro')
+    ('4', 'Escola municipal Henrique Talone Pinheiro')
+    ('5', 'Escola municipal de T.I. Vinícius de Moraes')
+    ('6', 'Escola municipal Beatriz Rodrigues da Silva')
+    ('7', 'Escola municipal Mestre Pacífico S. Campos')
+    ('8', 'Escola municipal Luiz Gonzaga')
+    ('9', 'Escola municipal de T.I. Padre Josimo M. Tavares')
+    ('10', 'Escola municipal de T.I. Daniel Batista')
+    ('11', 'Escola municipal de T.I. Monsenhor Pedro P. Piagem')
+    ('12', 'Escola municipal Jorge Amado')
+    ('13', 'Escola municipal Maria Rosa de Castro Sales')
+    ('14', 'Escola municipal Professor Sávia F. Jacome')
+    ('15', 'Escola municipal de T.I. Caroline C. C. da Silva')
+    ('16', 'Escola municipal Aurélio Buarque de Holanda')
+    ('17', 'Escola municipal Maria Júlia Amorim Rodrigues')
+    ('18', 'Escola municipal Thiago Barbosa')
+    ('19', 'Escola municipal de T.I. Euridice F. de Mello')
+    ('20', 'Escola municipal de T.I. Margarida Gonçalves')
+    ('21', 'Escola municipal Crispim Pereira de Alencar')
+    ('22', 'Escola municipal de T.I. Aprigio T. de Matos')
+    ('23', 'Escola municipal de T.I. João Beltrão')
+    ('24', 'Escola municipal de T.I. Luiz Nunes de Oliveira')
+    ('25', 'Escola municipal de T.I. Sueli Pereira A. Reche')
+)
+
+PERIODO = (
+    ('0', 'Matutino')
+    ('1', 'Vespertino')
+    ('2', 'Noturno')
+    ('3', 'Tempo Integral')
+)
+
+TURMA = (
+    ('0', 'A')
+    ('1', 'B')
+    ('2', 'C')
+    ('3', 'D')
+)
+
+MUNICIPIO = (
+    ('0', 'Palmas')
+)
 
 
+# Constantes do modelo Questionário
 
 FREQUENCIA = (
     (0, 'NAO_SE_APLICA'),
@@ -22,3 +69,5 @@ FREQUENCIA = (
     (5, 'ALGUMAS_VEZES'),
     (6, 'QUASE_SEMPRE'),
 )
+
+
--- administracao/models.py
+++ administracao/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 class Aluno(models.Model):
     """Modelo que representa um aluno"""
-    nome = models.CharField(max_length=TAMANHO_NOMES, blank=True, null=True, verbose_name='Nome')
-    sexo = models.IntegerField(choices=SEXO, blank=True, null=True, verbose_name='Sexo')
-    escola = models.IntegerField(choices=ESCOLA, blank=True, null=True, verbose_name='Escola')
-    periodo = models.IntegerField(choices=PERIODO, blank=True, null=True, verbose_name='Período')
-    turma = models.IntegerField(choices=TURMA, blank=True, null=True, verbose_name='Turma')
-    municipio = models.IntegerField(choices=MUNICIPIO, blank=True, null=True, verbose_name='Município')
+    nome = models.CharField(max_length=TAMANHO_NOMES, verbose_name='Nome')
+    sexo = models.IntegerField(choices=SEXO, verbose_name='Sexo')
+    nascimento = models.DateField(verbose_name="Data de nascimento")
+    escola = models.IntegerField(choices=ESCOLA, verbose_name='Escola')
+    periodo = models.IntegerField(choices=PERIODO, verbose_name='Período')
+    turma = models.IntegerField(choices=TURMA, verbose_name='Turma')
+    municipio = models.IntegerField(choices=MUNICIPIO, verbose_name='Município')
 
     def __str__(self):
         """Devolve a representação do modelo em string"""
@@ -17,7 +18,59 @@ class Aluno(models.Model):
 
 class Questionario(models.Model):
     """Modelo que representa um questionário"""
-    questao1 = models.IntegerField(choices=QUESTAO, blank=True, null=True, verbose_name='Questão pessoal 1')
+    
+    # Informações pessoais
+    questao1 = models.IntegerField(choices=QUESTAO, verbose_name='1. Qual é a sua cor ou raça?')
+    questao2 = models.IntegerField(choices=QUESTAO, verbose_name='2. Qual é a sua idade?')
+    questao3 = models.IntegerField(choices=QUESTAO, verbose_name='3. Qual é o mês do seu aniversário?')
+    questao4 = models.IntegerField(choices=QUESTAO, verbose_name='4. Em que ano você nasceu?')
+    
+    # Informações gerais
+    questao5 = models.IntegerField(choices=QUESTAO, verbose_name='5. Qual é o seu gênero?')
+    questao6 = models.IntegerField(choices=QUESTAO, verbose_name='6. Em que ano/série você está?')
+    questao7 = models.ArrayField(models.IntegerField(choices=QUESTAO), default=[], verbose_name='7. Em que turno você estuda? (pode ser múltipla escola).')
+    questao8 = models.IntegerField(choices=QUESTAO, verbose_name='8. Você estuda em regime integral (tem atividades escolares por 7 horas ou mais diárias, durante todo o período escolar)?')
+    questao9 = models.IntegerField(choices=QUESTAO, verbose_name='9. Você estuda em regime de internato (a escola possui alojamento onde os alunos permanecem e dormem diariamente, durante todo o período escolar)?')
+    questao10 = models.IntegerField(choices=QUESTAO, verbose_name='10. Qual o grau de escolaridade mais elevado que você pretende concluir?')
+    questao11 = models.IntegerField(choices=QUESTAO, verbose_name='11. Quando terminar o ciclo/curso que você está frequentando atualmente, você pretende?')
+    questao12 = models.IntegerField(choices=QUESTAO, verbose_name='12. Você mora com sua mãe?')
+    questao13 = models.IntegerField(choices=QUESTAO, verbose_name='13. Você mora com seu pai?')
+    questao14 = models.IntegerField(choices=QUESTAO, verbose_name='14. Contando com você, quantas pessoas moram na sua casa ou apartamento?')
+    questao15 = models.IntegerField(choices=QUESTAO, verbose_name='15. Na sua casa tem telefone fixo (convencional)?')
+    questao16 = models.IntegerField(choices=QUESTAO, verbose_name='16. Você tem celular?')
+    questao17 = models.IntegerField(choices=QUESTAO, verbose_name='17. Na sua casa tem computador (de mesa, netbook, laptop etc.)?')
+    questao18 = models.IntegerField(choices=QUESTAO, verbose_name='18. Você tem acesso à internet em sua casa?')
+    questao19 = models.IntegerField(choices=QUESTAO, verbose_name='19. Alguém que mora na sua casa tem carro?')
+    questao20 = models.IntegerField(choices=QUESTAO, verbose_name='20. Alguém que mora na sua casa tem moto?')
+    questao21 = models.IntegerField(choices=QUESTAO, verbose_name='21. Quantos banheiros com chuveiro têm dentro da sua casa?')
+    questao22 = models.IntegerField(choices=QUESTAO, verbose_name='22. Tem empregado(a) doméstico(a) recebendo dinheiro para fazer o trabalho em sua casa, três ou mais dias por semana?')
+    questao23 = models.IntegerField(choices=QUESTAO, verbose_name='23. Qual nível de ensino (grau) sua mãe estudou ou estuda?')
+    questao24 = models.IntegerField(choices=QUESTAO, verbose_name='24. Você tem algum trabalho, emprego ou negócio atualmente?')
+    questao25 = models.IntegerField(choices=QUESTAO, verbose_name='25. Você recebe dinheiro por este trabalho, emprego ou negócio?')
+    questao26 = models.IntegerField(choices=QUESTAO, verbose_name='26. Sua família recebe benefícios do Programa Bolsa Família?')
+    
+    # Alimentação
+    questao27 = models.IntegerField(choices=QUESTAO, verbose_name='27. ')
+    
+    # Atividade física
+    
+    # Uso de cigarro
+    
+    # Bebidas alcoólicas
+    
+    # Drogas ilícitas
+    
+    # Situações em casa e na escola
+    
+    # Higiene e saúde bucal
+    
+    # Segurança
+    
+    # Uso de serviço de saúde
+    
+    # Imagem corporal
+    
+    # A sua opinião
 
     def __str__(self):
         """Devolve a representação do modelo em string"""
