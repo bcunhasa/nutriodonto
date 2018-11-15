a42abef29e78ecf6cf4d56f31053cbaee0ea7018
Bruno de Sá <bcunhasa@gmail.com>
2018-11-15 14:24:43 -0300

    Agora é possível editar corretamente os valores de questões multiplas

--- static/administracao/js/administracao.js
+++ static/administracao/js/administracao.js
@@ -10,17 +10,19 @@ function salvaQuestoes() {
 }
 
 function carregaQuestoes() {
-    var id_questao_146 = document.getElementById("id_questao_146").value;
+    // Carrega a questão 60
+    var questao_60 = JSON.parse("[" + document.getElementById("id_questao_60").value + "]");
+    var select_questao_60 = document.getElementById("select_questao_60");
+    
+    for (var i = 0; i < questao_60.length; i++) {
+        select_questao_60[questao_60[i]].selected = true;
+    }
+    
+    // Carrega a questão 146
+    var questao_146 = JSON.parse("[" + document.getElementById("id_questao_146").value + "]");
     var select_questao_146 = document.getElementById("select_questao_146");
     
-    console.log(id_questao_146);
-    console.log(id_questao_146.length);
-    
-    for (var j = 0; j < id_questao_146.length; j++) {
-        for (var i = 0; i < select_questao_146.length; i++) {
-            if (select_questao_146[i].innerHTML == id_questao_146[j]) {
-                select_questao_146[i].selected = true;
-            }
-        }
+    for (var i = 0; i < questao_146.length; i++) {
+        select_questao_146[questao_146[i]].selected = true;
     }
 }
