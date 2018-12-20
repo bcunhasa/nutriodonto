c4fef59b3749c878126f77f99bee39643620689d
Bruno de Sá <bcunhasa@gmail.com>
2018-12-20 02:14:24 -0300

    Customizações visuais na base do portal; agora todas as páginas do portal estão funcionando corretamente

--- portal/views.py
+++ portal/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 
+from gerencia.models import Noticia, Documento, Foto, Video
+
 
 class IndexView(View):
     """Página inicial do site"""
     
     def get(self, request):
-        return render(self.request, 'portal/index.html')
+        noticias = Noticia.objects.order_by('-id')
+        context = {'noticias': noticias}
+        return render(self.request, 'portal/index.html', context)
 
 
 class ProjetoView(View):
@@ -20,14 +24,19 @@ class DocumentosView(View):
     """Compatilhamento de documentos públicos"""
     
     def get(self, request):
-        return render(self.request, 'portal/documentos.html')
+        documentos = Documento.objects.order_by('-id')
+        context = {'documentos': documentos}
+        return render(self.request, 'portal/documentos.html', context)
 
 
 class MidiaView(View):
     """Exibição de mídias"""
     
     def get(self, request):
-        return render(self.request, 'portal/midia.html')
+        fotos = Foto.objects.order_by('-id')
+        videos = Video.objects.order_by('-id')
+        context = {'fotos': fotos, 'videos': videos}
+        return render(self.request, 'portal/midia.html', context)
 
 
 class ContatosView(View):
