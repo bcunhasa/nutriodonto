6bf9193867c76a9c27226ade2a5da5252f2c7450
Bruno de Sá <bcunhasa@gmail.com>
2019-10-01 17:13:11 -0300

    Adição de tabelas que exibem o CPO-D de acordo com as divisões solicitadas

--- estudo/views.py
+++ estudo/views.py
@@ -169,6 +169,56 @@ class EstudoView(LoginRequired, View):
             ['Zona rural', 0, 0, 0, 0],
         ]
         
+        # Questões sobre CPO-D (nome, dentes considerados, total de indivíduos)
+        cpod_geral = ['Geral', 0, 0]
+        cpod_escolas = [
+            ['Escola municipal de T.I. Padre Josimo M. Tavares',    0, 0],
+            ['Escola Municipal Beatriz Rodrigues da Silva',         0, 0],
+            ['Escola municipal Mestre Pacífico S. Campos',          0, 0],
+            ['Escola municipal Luiz Gonzaga',                       0, 0],
+            ['Escola municipal de T.I. Monsenhor Pedro P. Piagem',  0, 0],
+            ['Escola municipal de T.I. Daniel Batista',             0, 0],
+            ['Escola municipal Anne Frank',                         0, 0],
+            ['Escola municipal Henrique Talone Pinheiro',           0, 0],
+            ['Escola municipal de T.I. Aprigio T. de Matos',        0, 0],
+            ['Escola municipal de T.I. Vinícius de Moraes',         0, 0],
+            ['Escola municipal Darcy Ribeiro',                      0, 0],
+            ['Escola municipal Antônio G. de Carvalho Filho',       0, 0],
+            ['Escola municipal Antônio Carlos Jobim',               0, 0],
+            ['Escola municipal de T.I.Almirante Tamandare',         0, 0],
+            ['Escola municipal de T.I. Anisio Spenola Teixeira',    0, 0],
+            ['Escola municipal Thiago Barbosa',                     0, 0],
+            ['Escola municipal Aurélio Buarque de Holanda',         0, 0],
+            ['Escola municipal de T.I. Margarida Gonçalves',        0, 0],
+            ['Escola municipal Professor Sávia F. Jacome',          0, 0],
+            ['Escola municipal Jorge Amado',                        0, 0],
+            ['Escola municipal de T.I. Caroline C. C. da Silva',    0, 0],
+            ['Escola municipal Maria Rosa de Castro Sales',         0, 0],
+            ['Escola municipal de T.I. Euridice F. de Mello',       0, 0],
+            ['Escola municipal Maria Júlia Amorim Rodrigues',       0, 0],
+            ['Escola municipal Crispim Pereira de Alencar',         0, 0],
+            ['Escola municipal de T.I. Sueli Pereira A. Reche',     0, 0],
+            ['Escola municipal de T.I. Luiz Nunes de Oliveira',     0, 0],
+            ['Escola Municipal de Tempo Integral Marcos Freire',    0, 0],
+            ['Escola municipal de T.I. João Beltrão',               0, 0],
+        ]
+        cpod_territorios_saude = [
+            ['Kanela',     0, 0],
+            ['Apinajé',    0, 0],
+            ['Xambioá',    0, 0],
+            ['Krahô',      0, 0],
+            ['Karajá',     0, 0],
+            ['Javaé',      0, 0],
+            ['Xerente',    0, 0],
+            ['Panikararu', 0, 0],
+        ]
+        cpod_macrorregioes = [
+            ['Norte',      0, 0],
+            ['Centro sul', 0, 0],
+            ['Sul',        0, 0],
+            ['Zona rural', 0, 0],
+        ]
+        
         for exame in exames:
             # Idade (12, 13, 14, 15, 16, 17, 18 e 19)
             idade = int((datetime.date.today() - exame.aluno.nascimento).days / 365.25)
@@ -307,6 +357,97 @@ class EstudoView(LoginRequired, View):
             elif exame.aluno.escola.id == 64: sexo_escolas[27][coluna] += 1; sexo_macrorregioes[3][coluna] += 1; sexo_territorios_saude[7][coluna] += 1
             elif exame.aluno.escola.id == 51: sexo_escolas[28][coluna] += 1; sexo_macrorregioes[3][coluna] += 1; sexo_territorios_saude[7][coluna] += 1
             
+            # '1 - Cariado'; '4 - Perdido devido à cárie'; '5 - Perdido por outras razões'; '2 - Restaurado mas com cárie'; '3 - Restaurado e sem cárie'.
+            # '6 - Extração'.
+            entra_contagem = 0
+            escola = 0
+            macrorregiao = 0
+            territorio_saude = 0
+            
+            if exame.aluno.escola.id == 4: escola = 0; macrorregiao = 0; territorio_saude = 0
+            elif exame.aluno.escola.id == 65: escola = 1; macrorregiao = 0; territorio_saude = 0
+            elif exame.aluno.escola.id == 2: escola = 2; macrorregiao = 0; territorio_saude = 0
+            elif exame.aluno.escola.id == 1: escola = 3; macrorregiao = 0; territorio_saude = 0
+            elif exame.aluno.escola.id == 55: escola = 4; macrorregiao = 0; territorio_saude = 1
+            elif exame.aluno.escola.id == 48: escola = 5; macrorregiao = 0; territorio_saude = 1
+            elif exame.aluno.escola.id == 5: escola = 6; macrorregiao = 0; territorio_saude = 1
+            elif exame.aluno.escola.id == 6: escola = 7; macrorregiao = 1; territorio_saude = 1
+            elif exame.aluno.escola.id == 46: escola = 8; macrorregiao = 0; territorio_saude = 1
+            elif exame.aluno.escola.id == 59: escola = 9; macrorregiao = 1; territorio_saude = 2
+            elif exame.aluno.escola.id == 49: escola = 10; macrorregiao = 1; territorio_saude = 2
+            elif exame.aluno.escola.id == 44: escola = 11; macrorregiao = 1; territorio_saude = 3
+            elif exame.aluno.escola.id == 7: escola = 12; macrorregiao = 1; territorio_saude = 3
+            elif exame.aluno.escola.id == 62: escola = 13; macrorregiao = 1; territorio_saude = 3
+            elif exame.aluno.escola.id == 63: escola = 14; macrorregiao = 2; territorio_saude = 4
+            elif exame.aluno.escola.id == 57: escola = 15; macrorregiao = 2; territorio_saude = 4
+            elif exame.aluno.escola.id == 45: escola = 16; macrorregiao = 2; territorio_saude = 4
+            elif exame.aluno.escola.id == 8: escola = 17; macrorregiao = 2; territorio_saude = 4
+            elif exame.aluno.escola.id == 60: escola = 18; macrorregiao = 2; territorio_saude = 5
+            elif exame.aluno.escola.id == 50: escola = 19; macrorregiao = 2; territorio_saude = 5
+            elif exame.aluno.escola.id == 61: escola = 20; macrorregiao = 2; territorio_saude = 5
+            elif exame.aluno.escola.id == 54: escola = 21; macrorregiao = 2; territorio_saude = 5
+            elif exame.aluno.escola.id == 58: escola = 22; macrorregiao = 2; territorio_saude = 6
+            elif exame.aluno.escola.id == 53: escola = 23; macrorregiao = 2; territorio_saude = 6
+            elif exame.aluno.escola.id == 47: escola = 24; macrorregiao = 3; territorio_saude = 7
+            elif exame.aluno.escola.id == 56: escola = 25; macrorregiao = 3; territorio_saude = 7
+            elif exame.aluno.escola.id == 52: escola = 26; macrorregiao = 3; territorio_saude = 7
+            elif exame.aluno.escola.id == 64: escola = 27; macrorregiao = 3; territorio_saude = 7
+            elif exame.aluno.escola.id == 51: escola = 28; macrorregiao = 3; territorio_saude = 7
+            
+            if exame.carie_coroa_18 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_17 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_16 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_15 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_14 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_13 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_12 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_11 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_21 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_22 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_23 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_24 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_25 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_26 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_27 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_28 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_38 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_37 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_36 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_35 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_34 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_33 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_32 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_31 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_41 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_42 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_43 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_44 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_45 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_46 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_47 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            if exame.carie_coroa_48 == '1': entra_contagem = 1; cpod_geral[1] += 1; cpod_escolas[escola][1] += 1; cpod_macrorregioes[macrorregiao][1] += 1; cpod_territorios_saude[territorio_saude][1] += 1
+            
+            if entra_contagem == 1:
+                cpod_geral[2] += 1
+                cpod_escolas[escola][2] += 1
+                cpod_macrorregioes[macrorregiao][2] += 1
+                cpod_territorios_saude[territorio_saude][2] += 1
+        
+        if cpod_geral[2] != 0:
+            cpod_geral[1] /= cpod_geral[2]
+        
+        for escola in cpod_escolas:
+            if escola[2] != 0:
+                escola[1] /= escola[2]
+        
+        for macrorregiao in cpod_macrorregioes:
+            if macrorregiao[2] != 0:
+                macrorregiao[1] /= macrorregiao[2]
+        
+        for territorio_saude in cpod_territorios_saude:
+            if territorio_saude[2] != 0:
+                territorio_saude[1] /= territorio_saude[2]
+        
         context = {
             'pagina_estudo': True,
             
@@ -324,6 +465,11 @@ class EstudoView(LoginRequired, View):
             'sexo_escolas': sexo_escolas,
             'sexo_territorios_saude': sexo_territorios_saude,
             'sexo_macrorregioes': sexo_macrorregioes,
+            
+            'cpod_geral': cpod_geral,
+            'cpod_escolas': cpod_escolas,
+            'cpod_territorios_saude': cpod_territorios_saude,
+            'cpod_macrorregioes': cpod_macrorregioes,
         }
         
         return render(self.request, 'estudo/estudo.html', context)
